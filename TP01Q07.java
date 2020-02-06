44b41ce03e0fe5955de8d63dfb528621f4deda6f
phlc <pedrocarvalho@outlook.com>
2020-02-06 12:11:00 -0300

    TP01Q07.java leitura html OK

---
+++ TP01Q07.java
@@ -0,0 +1,61 @@
+/**
+* TP01 - Questao 7 - Leitura de Pagina HTML
+* @author Pedro Henrique Lima Carvalho
+* Matricula: 651230
+* AED2 - Tarde - Puc Minas
+*/
+
+import java.net.*;
+import java.io.*;
+
+public class TP01Q07{
+	
+	/**
+	* Metodo main
+	*/
+	public static void main (String[] args) throws Exception{
+		String nome = MyIO.readLine();
+		String endereco = MyIO.readLine();
+	//		while (!isEnd(nome) && !isEnd(endereco)){
+			MyIO.print(leitura(endereco));
+			MyIO.println(" "+nome);
+			nome = MyIO.readLine();
+			endereco = MyIO.readLine();
+	//	}
+	}
+	
+
+	/**
+	* isEnd - Verifica o fim das entradas
+	* @param String
+	* @return boolean
+	*/
+	public static boolean isEnd (String s){
+		return (s.length()==3 && s.charAt(0)=='F' &&
+			s.charAt(1)=='I' && s.charAt(2)=='M');
+	}
+
+	/**
+	*Leitura - Le uma pagina HTML como  String e retorna contagem
+	*@param String
+	*@return String
+	*/
+	public static String leitura (String endereco) throws Exception{
+		//declaracoes
+		String resp = "";
+		String linha";
+		String pagina = "";
+		URL site = new URL(endereco);
+		URLConnection conexao = site.openConnection();
+		BufferedReader entrada = new BufferedReader (new InputStreamReader(conexao.getInputStream()));
+		
+		while ((linha = entrada.readLine()) != null){
+			pagina = pagina + linha;
+
+/*
+		a(9360) e(11051) i(9618) o(5379) u(1560) á(112) é(146) í(49) ó(93) ú(9) à(6) è(0) ì(0) ò(0) ù(0) ã(248) õ(31) â(7) ê(38) î(0) ô(6) û(0) consoante(59353) <br>(0) <table>(0)
+*/
+		}
+		return resp;
+	}
+}
