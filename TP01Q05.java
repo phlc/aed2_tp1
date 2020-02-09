6924c9a2cb45e28c9af69f47e33d5b908fe33959
phlc <pedrocarvalho@outlook.com>
2020-02-08 22:21:49 -0300

    TP01Q05.java funcoes booleanas

--- TP01Q05.java
+++ TP01Q05.java
@@ -33,10 +33,55 @@ public class TP01Q05{
 	*@param String
 	*@return boolean
 	*/
-	public static String alter (String s){
+	public static String booleana (String s){
 		//declaracoes
-		boolean resp;
+		int n = (int) s.charAt(0);
+		int count = 2;
+		boolean[] variaveis = new boolean[n];
+		boolean resp = 0;	
+		char c;	
+
+		for (int i=0; i<n; i++){
+			
+			variaveis[i] = (int)s.charAt(count);
+			count = count + 2;
+		}
+		
+
+		while (count < s.length()){
+			c = s.charAt(count);
+		}
 	
+		
+			
 		return resp;
 	}
+
+	/**
+	*and - Retorna and de todas variaveis
+	*@param - booleans
+	*@return - boolean
+	*/
+	public static boolean and (boolean a, boolean b){
+		return (a&&b);
+	}
+
+	/**
+	*or - Retorna or de todas variaveis
+	*@param - booleans
+	*@return - boolean
+	*/
+	public static boolean or (boolean a, boolean b){
+		return (a||b);
+	}
+
+	/**
+	*not - Retorna not da variavel
+	*@param - boolean
+	*@return - boolean
+	*/
+	public static boolean not (boolean a){
+		return (!a);
+	}
+
 }
