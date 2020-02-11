7a0842893f88a6438b429c1c84a16733cb510ff4
phlc <pedrocarvalho@outlook.com>
2020-02-11 11:43:54 -0300

    TP01Q05.java igualado a TP01Q14.java

--- TP01Q05.java
+++ TP01Q05.java
@@ -9,11 +9,13 @@ class Equacao{
 	//atributos
 	private String s;
 	private int count;
+	private int limit;
 	private boolean[] vars;
 
 	//construtor
 	public Equacao(String str){
 		this.s = str;
+		this.limit = this.s.length();
 
 		int n = ((int) this.s.charAt(0)) - 48;
                 this.count = 2;
@@ -22,7 +24,7 @@ class Equacao{
                 for (int i=0; i<n; i++){
 
                         vars[i] = (((int)s.charAt(count)) - 48) == 1;
-                        count = count + 2;
+                       this. count = this.count + 2;
                 }
 	}
 
@@ -64,6 +66,14 @@ class Equacao{
 	}
 
 	/**
+	*getLimit - retorna o limite da string
+	*return in
+	*/
+	public int getLimit(){
+		return this.limit;
+	}
+
+	/**
 	*getValue - retorna o valor da variavel
 	*@param char
 	*@return boolean
@@ -75,7 +85,7 @@ class Equacao{
 	}
 }
 
-public class TP01Q05{
+public class TP01Q14{
 	
 	/**
 	* Metodo main
@@ -85,8 +95,8 @@ public class TP01Q05{
 		while (!isEnd(input)){	
 			if (booleana(input))
 				MyIO.println("1");	
-		//	else
-		//		MyIO.println("0");
+			else
+				MyIO.println("0");
 			
 			input = MyIO.readLine();
 		}
@@ -102,17 +112,44 @@ public class TP01Q05{
 		return (s.length()==1 && s.charAt(0)=='0');
 	}
 
+	
 	/**
 	*booleana - Resolve uma expressao booleana
 	*@param String
-	*@return boolean
+	*return boolean
 	*/
 	public static boolean booleana (String s){
-		//declaracoes
 		Equacao eq = new Equacao(s);
-		
+		return (booleana(eq));
+	}
+	
+	/**
+	*booleana - overload - Resolve uma expressao booleana
+	*@param Equacao
+	*@return boolean
+	*/
+	public static boolean booleana (Equacao eq){
+		//declaracoes
+		int init = eq.getCount();
+		int end = eq.getLimit();
+		boolean resp = false;
+		if (init<end){
+			if (eq.getChar() == 'a'){
+				resp =  and(eq);
+			}
+			else if (eq.getChar() == 'r'){
+				resp = or(eq);
+			}
+			else if (eq.getChar() == 't'){
+				resp = not(eq);
+			}
+			else{
+				eq.updateCount(1);
+				resp = booleana (eq);
+			}	
+		}
 			
-		return false;
+		return resp;
 	}
 
 	/**
@@ -124,7 +161,7 @@ public class TP01Q05{
 		boolean[] inputs = new boolean[5];
 		int input = 0;
 		for (int i=0; i<5; i++){
-			inputs[i] = true // valor neutro para AND
+			inputs[i] = true; // valor neutro para AND
 		}
 		eq.updateCount(1);
 
@@ -133,18 +170,19 @@ public class TP01Q05{
 				inputs[input] = eq.getValue(eq.getChar());
 				input++;
 			}
-			else if (eq.getChar() = 'a'){
+			else if (eq.getChar() == 'a'){
 				inputs[input] = and(eq);
 				input++;
 			}
-			else if (eq.getChar() = 'r'){
+			else if (eq.getChar() == 'r'){
 				inputs[input] = or(eq);
 				input++;
 			}
-			else if (eq.getChar() = 't'){
+			else if (eq.getChar() == 't'){
 				inputs[input] = not(eq);
 				input++;
 			}
+			eq.updateCount(1);
 		}
 		
 		return (inputs[0] && inputs[1] && inputs[2] && inputs[3] && inputs[4]);
@@ -159,7 +197,7 @@ public class TP01Q05{
 		boolean[] inputs = new boolean[5];
 		int input = 0;
 		for (int i=0; i<5; i++){
-			inputs[i] = false // valor neutro para OR
+			inputs[i] = false; // valor neutro para OR
 		}
 		eq.updateCount(1);
 
@@ -168,18 +206,20 @@ public class TP01Q05{
 				inputs[input] = eq.getValue(eq.getChar());
 				input++;
 			}
-			else if (eq.getChar() = 'a'){
+			else if (eq.getChar() == 'a'){
 				inputs[input] = and(eq);
 				input++;
 			}
-			else if (eq.getChar() = 'r'){
+			else if (eq.getChar() == 'r'){
 				inputs[input] = or(eq);
 				input++;
 			}
-			else if (eq.getChar() = 't'){
+			else if (eq.getChar() == 't'){
 				inputs[input] = not(eq);
 				input++;
 			}
+			
+			eq.updateCount(1);
 		}
 		
 		return (inputs[0] || inputs[1] || inputs[2] || inputs[3] || inputs[4]);
@@ -191,6 +231,28 @@ public class TP01Q05{
 	*@return boolean
 	*/
 	public static boolean not (Equacao eq){
+		boolean resp = false;
+		eq.updateCount(1);
 		
+		while (eq.getChar() != ')'){
+			if ('A'<=eq.getChar() && eq.getChar()<='Z'){
+				resp = eq.getValue(eq.getChar());
+			}
+			
+			else if (eq.getChar() == 'a'){
+				resp = and(eq);
+			}
+			else if (eq.getChar() == 'r'){
+				resp = or(eq);
+			}
+			else if (eq.getChar() == 't'){
+				resp = not(eq);
+			}
+		
+			eq.updateCount(1);
+			
+		}		
+		
+		return !resp;
 	}
 }
